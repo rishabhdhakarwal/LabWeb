61584e4dc42696d5cd101dbd1f748968b576ef74
rdhakarwal <rishabhdhakarwal@gmail.com>
2019-03-11 06:18:05 +0530

    server

---
+++ frontend/src/components/imageupload.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ImageUploader from 'react-images-upload';
+ 
+class Imageuploader extends React.Component {
+ 
+    constructor(props) {
+        super(props);
+         this.state = { pictures: [] };
+         this.onDrop = this.onDrop.bind(this);
+    }
+ 
+    onDrop(picture) {
+        this.setState({
+            pictures: this.state.pictures.concat(picture),
+        });
+    }
+ 
+    render() {
+        return (
+            <ImageUploader
+                
+                buttonText='Choose images'
+                onChange={this.onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+            />
+        );
+    }
+}
+
+export default ImageUploader;
+
+
+
